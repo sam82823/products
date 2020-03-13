fcb164c7c97d9704e38e828afcae4223d4ad70ee
Sam Li <sam82823@gmail.com>
2020-03-13 20:35:55 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+	name = input('Enter the product name: ')
+	if name == 'q': #quit
+		break
+	price = input('Enter the price: ')
+	# p = [] #p as products
+	# p.append(name) 
+	# p.append(price) #p清單內放著name and price
+    # p = [name, price] 這行等於第七到九行縮寫
+	# products.append(p) #把p裝進products這個大清單
+	products.append([name, price]) #直接把name跟price裝成小清單後裝到大清單products
+print(products)
